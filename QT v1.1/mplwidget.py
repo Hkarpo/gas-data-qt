94ab4e9a830e8c1583bdd6347c1426612793bcb8
YH <102796361+Hkarpo@users.noreply.github.com>
2024-01-15 19:31:03 +0800

    v1.1
    contains v1.0 and small changes.

---
+++ QT v1.1/mplwidget.py
@@ -0,0 +1,113 @@
+from PyQt5.QtWidgets import*
+
+from matplotlib.backends.backend_qt5agg import FigureCanvas
+
+from matplotlib.figure import Figure
+
+    
+class MplWidget1(QWidget):
+    
+    def __init__(self, parent = None):
+
+        QWidget.__init__(self, parent)
+        
+        self.canvas = FigureCanvas(Figure())
+        
+        vertical_layout = QVBoxLayout()
+        vertical_layout.addWidget(self.canvas)
+        
+        self.canvas.axes = self.canvas.figure.add_subplot(111)
+        self.setLayout(vertical_layout)
+        self.set_y()
+    def set_y(self):
+        self.canvas.axes.set_ylim(0, 2)
+
+
+class MplWidget2(QWidget):
+
+    def __init__(self, parent=None):
+        QWidget.__init__(self, parent)
+
+        self.canvas = FigureCanvas(Figure())
+
+        vertical_layout = QVBoxLayout()
+        vertical_layout.addWidget(self.canvas)
+
+        self.canvas.axes = self.canvas.figure.add_subplot(111)
+        self.setLayout(vertical_layout)
+        self.set_y()
+
+    def set_y(self):
+        self.canvas.axes.set_ylim(0, 2)
+
+
+class MplWidget3(QWidget):
+
+    def __init__(self, parent=None):
+        QWidget.__init__(self, parent)
+
+        self.canvas = FigureCanvas(Figure())
+
+        vertical_layout = QVBoxLayout()
+        vertical_layout.addWidget(self.canvas)
+
+        self.canvas.axes = self.canvas.figure.add_subplot(111)
+        self.setLayout(vertical_layout)
+        self.set_y()
+
+    def set_y(self):
+        self.canvas.axes.set_ylim(0, 2)
+
+
+class MplWidget4(QWidget):
+
+    def __init__(self, parent=None):
+        QWidget.__init__(self, parent)
+
+        self.canvas = FigureCanvas(Figure())
+
+        vertical_layout = QVBoxLayout()
+        vertical_layout.addWidget(self.canvas)
+
+        self.canvas.axes = self.canvas.figure.add_subplot(111)
+        self.setLayout(vertical_layout)
+        self.set_y()
+
+    def set_y(self):
+        self.canvas.axes.set_ylim(0, 2)
+
+
+class MplWidget5(QWidget):
+
+    def __init__(self, parent=None):
+        QWidget.__init__(self, parent)
+
+        self.canvas = FigureCanvas(Figure())
+
+        vertical_layout = QVBoxLayout()
+        vertical_layout.addWidget(self.canvas)
+
+        self.canvas.axes = self.canvas.figure.add_subplot(111)
+        self.setLayout(vertical_layout)
+        self.set_y()
+
+    def set_y(self):
+        self.canvas.axes.set_ylim(0, 2)
+
+
+class MplWidget6(QWidget):
+
+    def __init__(self, parent=None):
+        QWidget.__init__(self, parent)
+
+        self.canvas = FigureCanvas(Figure())
+
+        vertical_layout = QVBoxLayout()
+        vertical_layout.addWidget(self.canvas)
+
+        self.canvas.axes = self.canvas.figure.add_subplot(111)
+        self.setLayout(vertical_layout)
+        self.set_y()
+
+    def set_y(self):
+        self.canvas.axes.set_ylim(0, 2)
